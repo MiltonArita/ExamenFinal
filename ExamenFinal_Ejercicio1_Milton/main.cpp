8fda558c5660a57bdfbbc9a0037c77b945ff5b9b
MiltonArita <86279142+MiltonArita@users.noreply.github.com>
2021-09-22 18:46:31 -0600

    Add files via upload

---
+++ ExamenFinal_Ejercicio1_Milton/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	 int ops[2]={5,2};
+	 int i;
+	int opc;
+	cout<<"Mostrando Arreglo "<<ops[2]<<endl;
+	do{
+		cout<<"Ingrese Opciones"<<endl;
+		cout<<"1. C= Se elimina la puntación anterior "<<endl;
+		cout<<"2. D= Se duplica el último registro ingresado"<<endl;
+		cout<<"3. += Se calcula la sumatoria de todos las puntuaciones"<<endl;
+		
+		cin>>opc;
+		
+		switch(opc){
+		
+		case 1:
+			break;
+		case 2:
+		break;
+		case 3:
+		break;}	
+}
+while(opc!=4);
+	return 0;
+}
